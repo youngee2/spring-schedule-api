10286ce9df8eac89c9dde0ab18eba7538bb29fc2
youngee2 <jr3537@naver.com>
2025-05-10 14:31:14 +0900

    초기 환경설정 완료

---
+++ src/test/java/com/example/scheduleapi/ScheduleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
