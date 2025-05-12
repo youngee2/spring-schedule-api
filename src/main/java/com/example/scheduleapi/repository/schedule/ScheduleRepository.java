f245ae7d7267242fc226e870f64bf798323a9633
아영 <67311685+youngee2@users.noreply.github.com>
2025-05-13 00:32:22 +0900

    feat: 조건에 맞는 전체 일정 조회 기능 구현 (#4)
    feat: 조건에 맞는 전체 일정 조회 기능 구현 (#4)

--- src/main/java/com/example/scheduleapi/repository/schedule/ScheduleRepository.java
+++ src/main/java/com/example/scheduleapi/repository/schedule/ScheduleRepository.java
@@ -3,6 +3,8 @@ package com.example.scheduleapi.repository.schedule;
 import com.example.scheduleapi.domain.Schedule;
 import com.example.scheduleapi.dto.schedule.response.ScheduleResponseDto;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface ScheduleRepository {
@@ -10,4 +12,9 @@ public interface ScheduleRepository {
     ScheduleResponseDto saveSchedule(Schedule schedule);
     //단건 일정 조회
     Optional<ScheduleResponseDto> findById(Long id);
+    //전체 일정 조회
+    //조건에 해당하는 전체 일정 조회
+    List<ScheduleResponseDto> findAllFilterSchedules(String name, LocalDate updateAt);
+
+    List<ScheduleResponseDto> findAllSchedules();
 }
